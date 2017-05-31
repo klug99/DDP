b850cbed626ad0533e280522d86e495594b19470
Minseo Park <klug99@gmail.com>
2017-05-31 20:32:15 +0900

    BMI

---
+++ server.R
@@ -0,0 +1,17 @@
+library(shiny) 
+##Define the BMI function
+BMI<-function(weight,height) {weight/(height^2)}
+##defind value variable  returned from function
+result<-function(weight,height){
+  BMI_value<-weight/(height^2)
+  ifelse(BMI_value<18.5,"Underweight",ifelse(BMI_value<25,"Normal weight",ifelse(BMI_value<30,"Overweight","Obesity")))
+}
+##ShinyServer function
+shinyServer(
+  function(input, output) {
+    output$inputweightvalue <- renderPrint({input$weight})
+    output$inputheightvalue <- renderPrint({input$height})
+    output$estimation <- renderPrint({BMI(input$weight,input$height)})
+    output$diagnostic <- renderPrint({result(input$weight,input$height)})
+  } 
+)
---
+++ ui.R
@@ -0,0 +1,32 @@
+library(shiny) 
+shinyUI(
+  pageWithSidebar(
+    # Application title: Topic is calculating the body fat.
+    headerPanel("Body Mass Index (BMI) Calculator"),
+    ##Sidebar Panel for weight/height
+    sidebarPanel(
+      numericInput('weight', 'Insert your weight(kg)', 55) ,
+      numericInput('height', 'Insert your height(meter)', 1.70, min = 1.0, max = 2, step = 0.01),
+      submitButton('Submit')
+    ), 
+    # Main Panel: Print the body fat and description.
+    mainPanel(
+      p('The World Health Organization (WHO) announcement about body fat as below.'),
+      tags$div(
+        tags$ul(
+          tags$li('BMI <18.5       : Underweight'),
+          tags$li('BMI [18.5-24.9] : Normal weight'),
+          tags$li('BMI [25-29.9]   : Overweight'),
+          tags$li('BMI >=30        : Obesity')
+        )
+      ),
+      
+      h4('Taking into account the values entered by you:'), 
+      p('weight:'), verbatimTextOutput("inputweightvalue"),
+      p('height:'), verbatimTextOutput("inputheightvalue"),
+      p('Your BMI is:'),
+      verbatimTextOutput("estimation"),
+      p('It means that you are:'),strong(verbatimTextOutput("diagnostic"))
+    )
+  )   
+)
